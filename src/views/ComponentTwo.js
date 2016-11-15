94bee63e0ad940a87a3f73c548122b1b646f344a
DZuz <danielzuzevich@gmail.com>
2016-11-15 15:44:13 -0500

    Added IndexLink import to Component2 to showcase its use

--- src/views/ComponentTwo.js
+++ src/views/ComponentTwo.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { IndexLink } from 'react-router';
 
 
 class ComponentTwo extends React.Component {
     render() {
         return (
             <div>
-                React router has routed you to the a second component
-
+                React router has routed you to the a second component.
+                <br />
+                <IndexLink to="/"> Back To Main </IndexLink>
             </div>
         )
     }
