bb329aef2c0f8fe6a0521f275be4f8c3d9617aa9
DZuz <danielzuzevich@gmail.com>
2016-11-15 15:32:13 -0500

    start of redux setup

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import { render } from 'react-dom';
 import routes from './Routes';
 import './styles/main.scss'
 
+{/* Redux Specific */}
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './redux/reducers/';
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 window.React = React;
 render((
-   <Router history={browserHistory}>{routes}</Router>
-
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>
 ), document.getElementById('app'));
 
 // enable :active
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,8 @@
+// Just getting this Redux thing started
+
+import { combineReducers } from 'redux';
+const rootReducer = combineReducers({
+  state: (state = {}) => state
+});
+
+export default rootReducer;
